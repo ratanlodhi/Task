7dd7d2624de2c17b465362441a6af9e86bd2f760
Ratan Singh <ratansinghthakur28@gmail.com>
2025-08-25 01:54:42 +0530

    update

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,3 +1,7 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+// lib/supabaseClient.ts
+import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () => createClientComponentClient()
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
