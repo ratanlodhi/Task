563a36a4b9a8357f561c8c4c9bb8667c7c55bfbb
Ratan Singh <ratansinghthakur28@gmail.com>
2025-08-25 11:50:44 +0530

    Added supabase client file and updates

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import createClient from "@/lib/supabase/client";
+import { createClient } from "@/lib/supabase/client";
 import { Button } from '@/components/ui/button'
 import { 
   DropdownMenu, 
